5f9c7d61a01a6ab06a926d8fb732b44a69e07c12
Alex Lin <linalex7@protonmail.com>
2022-02-20 14:08:26 -0500

    fixing whitespace

--- bin/num_pictures.go
+++ bin/num_pictures.go
@@ -4,8 +4,8 @@ import (
     "fmt"
     "os"
     "io/ioutil"
-	"log"
-	"strings"
+    "log"
+    "strings"
 )
 
 /*
@@ -18,45 +18,45 @@ $ go run num_pictures.go /path
 
 func main() {
 
-	if len(os.Args[1:]) != 1 {
-		fmt.Println("\nUsage: ./num_pictuGres /path/to/directory\n")
-		os.Exit(1)
-	}
-	
+    if len(os.Args[1:]) != 1 {
+        fmt.Println("\nUsage: ./num_pictuGres /path/to/directory\n")
+        os.Exit(1)
+    }
+
     files, err := ioutil.ReadDir(os.Args[1])
     if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+        log.Fatal(err)
+        os.Exit(1)
     }
-	os.Chdir(os.Args[1])
-
+    os.Chdir(os.Args[1])
 
     for _, f := range files {
-		img_type := strings.SplitN(f.Name(), ".", -1)[1]
+        
+        img_type := strings.SplitN(f.Name(), ".", -1)[1]
 
-		r, _ := os.Open("/dev/urandom")
-		b := make([]byte, 5)
-		r.Read(b)
-		r.Close()
-		uuid := fmt.Sprintf("%x", b[0:4])
-		
-		new_name := fmt.Sprintf("%s.%s", uuid, img_type)
-		os.Rename(f.Name(), new_name)
+        r, _ := os.Open("/dev/urandom")
+        b := make([]byte, 5)
+        r.Read(b)
+        r.Close()
+        uuid := fmt.Sprintf("%x", b[0:4])
+
+        new_name := fmt.Sprintf("%s.%s", uuid, img_type)
+        os.Rename(f.Name(), new_name)
     }
 
     files, err = ioutil.ReadDir(os.Args[1])
     if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+        log.Fatal(err)
+        os.Exit(1)
     }
 
-	counter := 1
-	for _, f := range files{		
-		img_type := strings.SplitN(f.Name(), ".", -1)[1]
-		new_name := fmt.Sprintf("%d.%s", counter, img_type)
-		fmt.Printf("renaming %s --> %s\n", f.Name(), new_name)
-		os.Rename(f.Name(), new_name)
-		counter++
-	}
-
+    counter := 1
+    for _, f := range files{
+        img_type := strings.SplitN(f.Name(), ".", -1)[1]
+        new_name := fmt.Sprintf("%d.%s", counter, img_type)
+        fmt.Printf("renaming %s --> %s\n", f.Name(), new_name)
+        os.Rename(f.Name(), new_name)
+        counter++
+    }
 }
+
