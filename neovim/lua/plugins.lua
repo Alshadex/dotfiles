7cb1549b35507b2005f248a74bdc7c5f56373e5a
Alex Lin <alex.lin@Alex-MBP16.local>
2022-08-08 17:47:29 -0400

    fixed packer bootstrap a bit

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -2,9 +2,15 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
 	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.cmd [[packadd packer.nvim]]
 end
 
-return require('packer').startup(function()
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+	return
+end
+
+return require('packer').startup(function(use)
 
 	use 'wbthomason/packer.nvim'
 
@@ -20,24 +26,24 @@ return require('packer').startup(function()
 		},
 	}
 
-    -- use "lukas-reineke/lsp-format.nvim"
+	-- use "lukas-reineke/lsp-format.nvim"
 
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
 
-    use 'ray-x/go.nvim'
-    use 'ray-x/guihua.lua' -- recommanded if need floating window support
+	use 'ray-x/go.nvim'
+	use 'ray-x/guihua.lua' -- recommanded if need floating window support
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
-	-- if packer_bootstrap then
-	-- 	require('packer').sync()
-	-- end
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
