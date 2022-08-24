94aa41e22dd19dab511e0736ed434b9032b1dce4
Alex Lin <linalex7@protonmail.com>
2022-08-25 07:52:36 +0800

    Update keymaps

--- neovim/lua/core/keymaps.lua
+++ neovim/lua/core/keymaps.lua
@@ -1,4 +1,4 @@
-local opts = {noremap = false, silent = true}
+local opts = {silent = true}
 
 local keymap = vim.api.nvim_set_keymap
 
@@ -9,6 +9,11 @@ keymap('n', '<C-Up>', ':resize -1<CR>', opts)
 keymap('n', '<C-Down>', ':resize +1<CR>', opts)
 keymap('n', '<C-Left>', ':vertical resize -2<CR>', opts)
 keymap('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+keymap('n', '<C-h>', '<C-w>h', opts)
+keymap('n', '<C-j>', '<C-w>j', opts)
+keymap('n', '<C-k>', '<C-w>k', opts)
+keymap('n', '<C-l>', '<C-w>l', opts)
 
 --keymap('v', '<A-j>', ":m .+1<CR>==", opts)
 --keymap('v', '<A-k>', ":m .-2<CR>==", opts)
