b1f5d03f5e8ca015b5997523ba91cb546c1ae6a1
Alex Lin <linalex7@protonmail.com>
2025-01-13 15:45:26 +0800

    removing legacy scripts

--- bin/legacy/backup01.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rsync -rnv --delete --times ~/backup/0/ ~/backup/1/
-
-read -p "Are you sure? ([y]es or [N]o): " -n 2 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	rsync -rv --progress --delete --times ~/backup/0/ ~/backup/1/
-fi
