9993202faec1c27f81860ac94e7a26dc7fa36b16
Alex Lin <linalex7@protonmail.com>
2025-06-03 22:40:08 -0700

    Improved git-status-check script output with color and formatting.

--- bin/git-status-check.sh
+++ bin/git-status-check.sh
@@ -11,22 +11,31 @@ fi
 
 
 for repo in $(find $repos_dir -maxdepth 1 -mindepth 1 -type d); do
-    echo "Checking $repo"
+    # Colors
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[1;33m'
+    BLUE='\033[0;34m'
+    NC='\033[0m' # No Color
+
+    echo -e "${BLUE}Checking $repo${NC}"
     cd $repo
-        echo ""
+    echo ""
     status=$(git status -s)
     code=$?
 
     if [ $code -ne 0 ]; then
-        echo "  Repository $repo has an error:"
-        echo "  $status"
+        echo -e "  ${RED}Repository $repo has an error:${NC}"
+        echo -e "  ${YELLOW}$status${NC}"
         echo ""
         continue
     fi
 
     if [ -n "$status" ]; then
-        echo "  Repository $repo has uncommitted changes:"
-        echo "  $status"
+        echo -e "  ${YELLOW}Repository $repo has uncommitted changes:${NC}"
+        echo -e "  ${RED}$status${NC}"
+    else
+        echo -e "  ${GREEN}Repository $repo is clean.${NC}"
     fi
     echo ""
 done
