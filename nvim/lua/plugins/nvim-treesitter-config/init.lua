df4afc7d228f493dae292bd1088eadf76c044f35
Alex Lin <private.linalex@protonmail.com>
2026-06-14 14:16:44 -0700

    updating lazy nvim modules and fixing nvim-treesitter config

--- nvim/lua/plugins/nvim-treesitter-config/init.lua
+++ nvim/lua/plugins/nvim-treesitter-config/init.lua
@@ -1,5 +1,5 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = { 
+require('nvim-treesitter').setup({
+    ensure_installed = {
         'c', 'cpp', 'make',
         'go', 'gomod', 'gowork',
         'javascript', 'typescript',
@@ -10,14 +10,6 @@ require'nvim-treesitter.configs'.setup {
         'solidity',
         'lua',
         'html', 'css',
-        'hcl'
+        'hcl',
     },
-    highlight = {
-    enable = true,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-    },
-}
+})
